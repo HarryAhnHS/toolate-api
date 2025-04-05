c433aa2c178f1b9744b73cc7befde4d92d016dbe
Hongsuh Ahn <hongsuhahn@gmail.com>
2025-04-05 16:37:22 -0700

    api: cors middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import query, analyze 
+import os
 
 app = FastAPI()
+
+FRONTEND_URL = os.getenv("FRONTEND_URL") or "http://localhost:3000"
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=FRONTEND_URL,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(query.router, prefix="/api")
 app.include_router(analyze.router, prefix="/api")
