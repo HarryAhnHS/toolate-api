8edebf78c2d475a98dff999f9b4fe3d168128dc5
Hongsuh Ahn <hongsuhahn@gmail.com>
2025-04-07 02:34:51 -0700

    fix: cors middleware trailing slash from frontend url

--- app/main.py
+++ app/main.py
@@ -5,11 +5,11 @@ import os
 
 app = FastAPI()
 
-FRONTEND_URL = os.getenv("FRONTEND_URL") or "http://localhost:3000"
+FRONTEND_URL = (os.getenv("FRONTEND_URL") or "http://localhost:3000").rstrip("/")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL, "http://localhost:3000"],
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
